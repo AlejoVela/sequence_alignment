25f423bfae2da96e3e3f50bb775e5d9ef5104e2e
Diego Restrepo <alejorvela@gmail.com>
2022-06-08 19:44:10 -0500

    fix: validations and global alignments are fixed

--- main.js
+++ main.js
@@ -10,17 +10,26 @@ let $chainTwoLimit1 = document.getElementById("chainTwoLimit1");
 let $chainTwoLimit2 = document.getElementById("chainTwoLimit2");
 let $resultText = document.getElementById("resultText");
 let $messageLabel = document.getElementById("messageLabel");
-let regularExpression = /^[GCTA]+$/i;
-let isLocalAligment = false;
+var $snackbar = document.getElementById("snackbar");
+let regularExpression = /^[GCTA]{30,60}$/i;
+let completeChain = "";
+let isLocalAlignment = false;
 let sameSize = false;
 let points = 0;
 
-const GlobalAlignment = (sequenceOne, sequenceTwo, aligment) => {
+const GlobalAlignment = (sequenceOne, sequenceTwo, alignment) => {
+  let localSizeChainOne = sequenceOne.length;
+  let localSizeChainTwo = sequenceTwo.length;
   let chainOneHtml = "<th>Chain 1</th>";
   let chainTwoHtml = "<th>Chain 2</th>";
   let chainCoincidence = "<th>#</th>";
-
-  for (var i = 0; i < LowestSize(sequenceOne, sequenceTwo); i++) {
+  let size = DefineLowestChain(sequenceOne, sequenceTwo);
+  if (sequenceOne.length != sequenceTwo.length) {
+    sequenceOne.length > sequenceTwo.length
+      ? (sequenceTwo += completeChain)
+      : (sequenceOne += completeChain);
+  }
+  for (var i = 0; i < size; i++) {
     chainOneHtml += `<td>${sequenceOne[i]}</td>`;
     chainTwoHtml += `<td>${sequenceTwo[i]}</td>`;
     if (sequenceOne[i] === sequenceTwo[i]) {
@@ -30,35 +39,78 @@ const GlobalAlignment = (sequenceOne, sequenceTwo, aligment) => {
       chainCoincidence += `<td class="table-danger"></td>`;
     }
   }
-  let resultText = `${aligment}\nCantidad de coincidencias: ${points}`; 
-  sameSize == true ? resultText += `\nPorcentaje de Similitud ${(points*100)/sequenceOne.length}%` : resultText += "";
-  resultText += `\nLogintud cadena 1: ${sequenceOne.length}\nLogintud cadena 2: ${sequenceTwo.length}`
+  let resultText = `${alignment}\nCantidad de coincidencias: ${points}`;
+  sameSize == true
+    ? (resultText += `\nPorcentaje de Similitud ${
+        (points * 100) / sequenceOne.length
+      }%`)
+    : (resultText += "");
+  resultText += `\nLogintud cadena 1: ${$chainOne.value.length}\nLogintud cadena 2: ${$chainTwo.value.length}`;
+  isLocalAlignment == true
+    ? (resultText += `\nLogintud local cadena 1: ${localSizeChainOne}\nLogintud local cadena 2: ${localSizeChainTwo}`)
+    : (resultText += "");
+    isLocalAlignment == true
+    ? PrintWithSnackBar(
+        "Exito!", 
+        "Alineamiento Local Exitoso!",
+        "success", 
+        6000
+      )
+    : PrintWithSnackBar(
+        "Exito!", 
+        "Alineamiento global Exitoso!",
+        "success", 
+        6000
+      );
   $resultChainOne.innerHTML = chainOneHtml;
   $resultChainTwo.innerHTML = chainTwoHtml;
   $resultCoincidence.innerHTML = chainCoincidence;
   $resultText.innerText = resultText;
 };
 
-const IsLocalAligment = (sequenceOne, sequenceTwo) => {
-  let subSequenceOne = sequenceOne.substring($chainOneLimit1.value, parseInt($chainOneLimit2.value) + 1);
-  let subSequenceTwo = sequenceTwo.substring($chainTwoLimit1.value, parseInt($chainTwoLimit2.value) + 1);
+const IsLocalAlignment = (sequenceOne, sequenceTwo) => {
+  let subSequenceOne = sequenceOne.substring(
+    $chainOneLimit1.value,
+    parseInt($chainOneLimit2.value) + 1
+  );
+  let subSequenceTwo = sequenceTwo.substring(
+    $chainTwoLimit1.value,
+    parseInt($chainTwoLimit2.value) + 1
+  );
   subSequenceOne.length == subSequenceTwo.length
     ? (sameSize = true)
     : (sameSize = false);
-  GlobalAlignment(subSequenceOne, subSequenceTwo,"Alineamiento Local");
-}
+  GlobalAlignment(subSequenceOne, subSequenceTwo, "Alineamiento Local");
+};
 
 const PrintMessage = (message, color) => {
   $messageLabel.innerHTML = message;
   if (color) {
-    $messageLabel.style.color = "green";
+    $messageLabel.style.color = "DarkGoldenRod";
   } else {
     $messageLabel.style.color = "red";
   }
 };
 
+const DefineLowestChain = (chainOne, chainTwo) => {
+  let lowestSize = LowestSize(chainOne, chainTwo);
+  let highestSize = HighestSize(chainOne, chainTwo);
+  completeChain = "";
+  for (let i = 0; i < highestSize - lowestSize; i++) {
+    completeChain += " ";
+  }
+
+  return highestSize;
+};
+
 const LowestSize = (chainOne, chainTwo) => {
-  let size = chainOne.length > chainTwo.length ? chainTwo.length : chainOne.length;
+  let size =
+    chainOne.length > chainTwo.length ? chainTwo.length : chainOne.length;
+  return size;
+};
+const HighestSize = (chainOne, chainTwo) => {
+  let size =
+    chainOne.length > chainTwo.length ? chainOne.length : chainTwo.length;
   return size;
 };
 
@@ -66,7 +118,14 @@ const Validations = (sequenceOne, sequenceTwo) => {
   sequenceOne.length == sequenceTwo.length
     ? (sameSize = true)
     : (sameSize = false);
-  if(
+
+  if (
+    $chainOneLimit1.value != "" ||
+    $chainOneLimit2.value != "" ||
+    $chainTwoLimit1.value != "" ||
+    $chainTwoLimit2.value != ""
+  ) {
+    if (
       $chainOneLimit1.value == "" ||
       $chainOneLimit1.value < 0 ||
       $chainOneLimit1.value > sequenceOne.length ||
@@ -81,12 +140,50 @@ const Validations = (sequenceOne, sequenceTwo) => {
       $chainTwoLimit2.value > sequenceTwo.length ||
       $chainOneLimit1.value > $chainOneLimit2.value ||
       $chainTwoLimit1.value > $chainTwoLimit2.value
-    ){
-      isLocalAligment = false;
+    ) {
+      if($chainOneLimit1.value > $chainOneLimit2.value ||
+        $chainTwoLimit1.value > $chainTwoLimit2.value){
+          PrintWithSnackBar(
+            "Advertencia para realizar un alineamiento local", 
+            "Para realizar un alineamiento local," + 
+            "<br>las posiciones de inicio deben ser <br>"+
+            "menores a las posiciones finales" +
+            "<br>para ambas cadenas",
+            "warning", 
+            6000
+            )
+            PrintMessage("Para realizar un alineamiento local las posiciones de inicio deben ser menores a las posiciones finales para ambas cadenas", true);
+        } else {
+          PrintWithSnackBar(
+            "Advertencia para realizar un alineamiento local", 
+            "Para realizar un alineamiento local," + 
+            "<br>Asegurese de que las posiciones esten correctamente" + 
+            "<br>diligenciadas para ambas cadenas.",
+            "warning", 
+            6000
+            )
+        }
+
+      isLocalAlignment = false;
     } else {
-      isLocalAligment = true;
+      isLocalAlignment = true;
     }
-}
+  } else {
+    isLocalAlignment = false;
+  }
+};
+
+const PrintWithSnackBar = (title, message, type, time) => {
+  $snackbar.innerHTML = `${title}<hr>${message}`;
+  $snackbar.className = `show ${type}`;
+
+  setTimeout(function () {
+    $snackbar.className = $snackbar.className.replace("show", "");
+    $snackbar.className = $snackbar.className.replace("danger", "");
+    $snackbar.className = $snackbar.className.replace("success", "");
+    $snackbar.className = $snackbar.className.replace("warning", "");
+  }, time);
+};
 
 $sendButton.onclick = function () {
   points = 0;
@@ -100,15 +197,29 @@ $sendButton.onclick = function () {
       regularExpression.test(sequenceTwo)
     ) {
       Validations(sequenceOne, sequenceTwo);
-      isLocalAligment == false ? GlobalAlignment(sequenceOne, sequenceTwo, "Alineamiento Global") : IsLocalAligment(sequenceOne, sequenceTwo);
+      isLocalAlignment == false
+        ? GlobalAlignment(sequenceOne, sequenceTwo, "Alineamiento Global")
+        : IsLocalAlignment(sequenceOne, sequenceTwo);
     } else {
       PrintMessage(
-        "GCTA son los unicos caracteres validos para una secuencia",
+        "GCTA son los unicos caracteres validos para una secuencia", 
         false
       );
-      console.log("NO PASO!");
+      PrintWithSnackBar(
+        "Error en las cadenas ingresadas", 
+        "GCTA son los unicos caracteres validos para una secuencia" + 
+        "<br>Ademas, estas debem temer una longitud entre 30 y 40 caracteres",
+        "danger", 
+        6000
+        )
     }
   } else {
     PrintMessage("Por favor ingrese las dos secuencias correctamente", false);
+    PrintWithSnackBar(
+      "Error en las cadenas ingresadas", 
+      "Por favor ingrese las dos secuencias correctamente",
+      "danger", 
+      6000
+      )
   }
 };
